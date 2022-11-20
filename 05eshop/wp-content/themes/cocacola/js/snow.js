56f4893004501102244140bf8ca7ae58f6b0c06e
Ninkame <113964617+Ninkame@users.noreply.github.com>
2022-11-20 11:50:21 +0100

    Delete 05eshop directory

--- 05eshop/wp-content/themes/cocacola/js/snow.js
+++
@@ -1,9 +0,0 @@
-$('video').hide();
-document.querySelector('video').playbackRate = 1;
-
-
-$('video').fadeIn(1000);
-
-let intervalId = window.setInterval(function(){
-    $('video').fadeOut(1000).fadeIn(500);
-  }, 15000);
